5923ddda5e744be90eb6a38916cfcd6ceca5c2a6
Muhammad Faiz <mfcc64@gmail.com>
2022-09-03 16:20:39 +0700

    package.json: add dependencies and fix-import

---
+++ fix-import.js
@@ -0,0 +1,7 @@
+
+const fs = require("fs");
+const url = "https://cdn.jsdelivr.net/npm/showcqt@1.1.2/showcqt.mjs";
+const filename = "showcqt-element.mjs";
+const str = fs.readFileSync(filename, "utf8");
+const new_str = str.replace(url, "showcqt");
+fs.writeFileSync(filename, new_str, "utf8");
