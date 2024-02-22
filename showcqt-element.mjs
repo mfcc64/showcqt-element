6b6fb5f523c6e792e1caebd1fda2a80d35e4ca3c
Muhammad Faiz <mfcc64@gmail.com>
2024-02-22 11:12:39 +0700

    package.json: prepare for version 2.0.0

--- showcqt-element.mjs
+++ showcqt-element.mjs
@@ -38,7 +38,7 @@ const OBSERVED_ATTRIBUTES = {
 // Hopefully nobody hijacks HTMLDivElement
 const HTMLElement = Object.getPrototypeOf(HTMLDivElement);
 class ShowCQTElement extends HTMLElement {
-    static version = "1.3.1";
+    static version = "2.0.0";
 
     static global_audio_context;
 
