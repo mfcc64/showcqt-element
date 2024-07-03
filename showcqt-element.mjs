ae87e4a08839b2ff6b875aec120f70622853ba91
Muhammad Faiz <mfcc64@gmail.com>
2024-07-03 13:21:17 +0700

    package.json: increment version to 2.0.1

--- showcqt-element.mjs
+++ showcqt-element.mjs
@@ -3,7 +3,7 @@
 // preload
 import "../showcqt@2/showcqt.mjs";
 
-// version 2.0.0
-import "../showcqt-element@2.0.0/extern.mjs";
-import "../showcqt-element@2.0.0/extern-cdn.mjs";
-export * from "../showcqt-element@2.0.0/showcqt-element-main.mjs";
+// version 2.0.1
+import "../showcqt-element@2.0.1/extern.mjs";
+import "../showcqt-element@2.0.1/extern-cdn.mjs";
+export * from "../showcqt-element@2.0.1/showcqt-element-main.mjs";
